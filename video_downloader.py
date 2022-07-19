49f65397c7b0ca55775caa901b73768dec2a7471
Fittafa <83968590+Fittafa@users.noreply.github.com>
2022-07-19 18:40:08 +0300

    Update video_downloader.py

--- video_downloader.py
+++ video_downloader.py
@@ -1,12 +1,17 @@
-# High Quality Youtube Video Downloader with pytube and moviepy
+# High Quality YouTube Video Downloader with pytube and moviepy
 
-from pytube import YouTube
-from moviepy.editor import VideoFileClip, AudioFileClip, CompositeAudioClip
-from sys import argv
 import os
+from pytube import YouTube
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.audio.AudioClip import CompositeAudioClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
 
 # Takes the link from the user
-link = input("Please, enter link: ")
+link = input("Enter the link: ")
+video_name = input("Enter the video name: ")
+
+#takes the location from the user
+location = input("Enter the folder location: ")
 
 yt = YouTube(link)
 
@@ -27,19 +32,18 @@ highest_video = stream[0]
 highest_audio = stream[-1]
 
 # Downloads the video and audio, ADD THE LOCATION
-highest_video.download(output_path='Location of the video', filename="video")
-highest_audio.download(output_path='Location of the audio', filename="audio" )
+highest_video.download(output_path=location, filename="video")
+highest_audio.download(output_path=location, filename="audio")
 
-clip1 = VideoFileClip("video")
+clip1 = VideoFileClip(rf"{location}/video")
 
-audio = AudioFileClip("audio")
+audio = AudioFileClip(rf"{location}/audio")
 
 # Merges the video and audio
 clip1.audio = CompositeAudioClip([audio])
 
-clip1.write_videofile(f"{name}.mp4")
+clip1.write_videofile(rf"{location}/{video_name}.mp4")
 
 # Removes the single files, ADD THE LOCATION
-for file in os.listdir('Location of the video and audio'):
-    os.remove("video")
-    os.remove("audio")
+os.remove(rf"{location}/video")
+os.remove(rf"{location}/audio")
